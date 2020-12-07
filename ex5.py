b6e9a65141e5804365de84079bff9a9d992e3f55
Kim Tae Hyun <birdman@Kims-MacBook-Pro.local>
2020-12-07 23:25:43 +0800

    Learn python 3 the hard way

---
+++ ex5.py
@@ -0,0 +1,20 @@
+my_name = 'Kim'
+my_age = 23
+my_height = 178 #cm
+my_weight = 74 #kgs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+# f"Hello {my_name}"
+# This f before the " tells python3 that this string needs to be formatted.
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} kgs heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
