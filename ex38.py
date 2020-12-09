dd6af4067720c87904bb2be5638a4a05c7eb7d71
Kim Tae Hyun <birdman@Kims-MacBook-Pro.local>
2020-12-10 00:21:11 +0800

    Learn python 3 the hard way

---
+++ ex38.py
@@ -0,0 +1,22 @@
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait there are not 10 things in that list. Let's fix that.")
+
+stuff = ten_things.split(' ')       # it makes the variable 'stuff' a list.
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()     # it pops an element from more_stuff list and stores it into next_one.
+    print("Adding: ", next_one)
+    stuff.append(next_one)
+    print(f"There are {len(stuff)} items now.")
+
+print("There we go: ", stuff)
+
+print("Let's do some things with stuff.")
+
+print(stuff[1])
+print(stuff[-1])
+print(stuff.pop())
+print(' '.join(stuff))
+print('#'.join(stuff[3:5]))
